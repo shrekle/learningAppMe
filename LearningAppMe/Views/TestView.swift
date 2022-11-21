701e080e1f37b49a9c32df5fd3280316404bc673
shrekle <nanonline760@gmail.com>
2022-11-21 13:50:27 -0900

    submit button in testView highlight appropriate colors

--- LearningAppMe/Views/TestView.swift
+++ LearningAppMe/Views/TestView.swift
@@ -11,25 +11,86 @@ struct TestView: View {
     
     @EnvironmentObject var model: ContentModel
     
+    @State private var selectedAnswerIndex: Int?
+    @State private var submitted = false
+    
+    @State private var numCorrect = 0
+    
     var body: some View {
         
         VStack {
             if model.currentQuestion != nil {
-                VStack {
+                VStack(alignment: .leading) {
                     // question number
                     Text("Question \(model.currentQuestionIndex + 1) of \(model.currentModule?.test.questions.count ?? 0)")
                     
                     // question
                     CodeTextView()
-                    // answers
                     
-                    // button
+                    // Answers Button
+                    ScrollView {
+                        VStack {
+                            ForEach(0..<model.currentQuestion!.answers.count, id: \.self) { i in
+                                
+                                Button {
+                                    selectedAnswerIndex = i
+                                } label: {
+                                    
+                                    ZStack {
+                                        if submitted == false {
+                                            
+                                            RectangleCard(color:  (selectedAnswerIndex == i) ? .gray : .white)
+                                            
+                                        } else {
+                                            
+                                            if (i == selectedAnswerIndex && i == model.currentQuestion!.correctIndex) || i == model.currentQuestion!.correctIndex {
+                                                
+                                                RectangleCard(color: .green)
+                                                
+                                            } else if i == selectedAnswerIndex && i != model.currentQuestion!.correctIndex {
+                                                
+                                                RectangleCard(color: .red)
+                                            } else {
+                                                RectangleCard(color: .white)
+                                            }
+                                        }
+                                        Text(model.currentQuestion!.answers[i])
+                                    }
+                                    .frame(height: 48)
+                                    .padding(.horizontal)
+                                }
+                                .disabled(submitted)
+                            }
+                        }
+                        .tint(.black)
+                        .padding(.vertical)
+                    }
+                    
+                    // submit button
+                    Button {
+                        submitted = true
+                        if selectedAnswerIndex == model.currentQuestion!.correctIndex {
+                            numCorrect += 1
+                        }
+                    } label: {
+                        ZStack {
+                            RectangleCard(color: .green)
+                                .frame(height: 48)
+                            
+                            Text("Submit")
+                                .tint(.white)
+                                .bold()
+                        }
+                        .padding(.horizontal)
+                    }
+                    .disabled(selectedAnswerIndex == nil ? true : false)
                 }
             }
             else {
                 ProgressView()
             }
         }
+        .padding(.horizontal)
         .navigationTitle("\(model.currentModule?.category ?? "") Test")
     }
 }
